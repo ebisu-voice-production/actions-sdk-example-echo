186cf920fddc85d68753a4efa614b22c969ceaab
tell-y <t.yamamoto28@gmail.com>
2018-04-13 23:49:29 +0900

    basic setting

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Echo Example
 ## Set up
 - Run `$ npm run firebase -- login`
-- Run `$ npm run firebase -- init functions`
+- Create `action.json` based on `action.template.json` (Replace LOCALE and YOUR_ENDPOINT_URL with yours)
---
+++ src/index.js
@@ -0,0 +1,38 @@
+const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
+const functions = require('firebase-functions');
+
+const NO_INPUTS = ["I didn't hear that."];
+
+exports.echo = functions.https.onRequest((request, response) => {
+  const app = new ActionsSdkApp({ request, response });
+
+  const mainIntent = (app) => {
+    const inputPrompt = app.buildInputPrompt(
+      true,
+      '<speak>Hi!</speak>',
+      NO_INPUTS
+    );
+    app.ask(inputPrompt);
+  };
+
+  const rawInput = (app) => {
+    if (app.getRawInput() === 'bye') {
+      app.tell('Goodbye!');
+    } else {
+      const inputPrompt = app.buildInputPrompt(
+        true,
+        '<speak>You said, ' +
+          app.getRawInput() +
+          '</speak>',
+        NO_INPUTS
+      );
+      app.ask(inputPrompt);
+    }
+  }
+
+  const actionMap = new Map();
+  actionMap.set(app.StandardIntents.MAIN, mainIntent);
+  actionMap.set(app.StandardIntents.TEXT, rawInput);
+
+  app.handleRequest(actionMap);
+});
