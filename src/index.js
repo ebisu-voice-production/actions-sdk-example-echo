85b6614fdd5b687046d3e4a33b1fe9f92892a30f
tell-y <t.yamamoto28@gmail.com>
2018-04-15 00:47:26 +0900

    add eslint

--- src/index.js
+++ src/index.js
@@ -2,35 +2,27 @@ const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
 const functions = require('firebase-functions');
 
 const NO_INPUTS = ["I didn't hear that."];
-
-exports.echo = functions.https.onRequest((request, response) => {
-  const app = new ActionsSdkApp({ request, response });
-
-  const mainIntent = (app) => {
+const mainIntent = app => {
+  const inputPrompt = app.buildInputPrompt(true, '<speak>Hi!</speak>', NO_INPUTS);
+  app.ask(inputPrompt);
+};
+const rawInput = app => {
+  if (app.getRawInput() === 'bye') {
+    app.tell('Goodbye!');
+  } else {
     const inputPrompt = app.buildInputPrompt(
       true,
-      '<speak>Hi!</speak>',
+      `<speak>You said, ${app.getRawInput()}</speak>`,
       NO_INPUTS
     );
     app.ask(inputPrompt);
-  };
-
-  const rawInput = (app) => {
-    if (app.getRawInput() === 'bye') {
-      app.tell('Goodbye!');
-    } else {
-      const inputPrompt = app.buildInputPrompt(
-        true,
-        `<speak>You said, ${app.getRawInput()}</speak>`,
-        NO_INPUTS
-      );
-      app.ask(inputPrompt);
-    }
   }
+};
 
+exports.echo = functions.https.onRequest((request, response) => {
+  const app = new ActionsSdkApp({ request, response });
   const actionMap = new Map();
   actionMap.set('actions.intent.MAIN', mainIntent);
   actionMap.set('actions.intent.TEXT', rawInput);
-
   app.handleRequest(actionMap);
 });
